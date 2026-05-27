81426e66d73dd623597174d85e8efba1faa052b8
tilahun2121 <tilahun2014cbe@gmail.com>
2026-05-27 15:17:50 +0300

    Fix case sensitivity for carousel.css

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { img } from "./img/data";
-import './Carousel.css';
+import './carousel.css';
 
 function Carouseleffect() {
   return (
